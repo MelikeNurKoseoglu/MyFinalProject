d0081164c35bf9c978f44202231cf3fad7e07d9f
Melike Nur Köseoğlu <koseoglumelikenur@gmail.com>
2022-05-13 17:37:30 +0300

    Add project files.

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "ürün eklendi";
+        public static string ProductNameInvalid = "ürün ismi geçersiz";
+        public static string MaintenanceTime="sistem bakımda";
+        public static string ProductsListed="ürünler listelendi";
+    }
+}
